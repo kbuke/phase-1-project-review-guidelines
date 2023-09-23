ae02ca54522bd2b9db4023ad3416121530e0b01c
kaan <kabuke13@gmail.com>
2023-09-23 10:22:44 +0100

    I have created variable pokeImg which stores all images of the 151 Pokemon

--- index.js
+++ index.js
@@ -44,12 +44,19 @@ document.addEventListener("DOMContentLoaded", () => {
       pokeCardDiv.id = kantoId
       // pokeId.innerText = `KANTO REGION ID: ${pokeInfo.id}`
 
+      //Get the image of each of the 151 Pokemon
+      let pokeImg = document.createElement("img")
+      pokeImg.className = "pokemonImages"
+      pokeImg.src = `${pokeInfo.sprites["front_default"]}`
+
       //Create the back of the Pokemon Card
       let backOfPokeCard = document.createElement("img")
       backOfPokeCard.className = "backEnd"
       backOfPokeCard.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4f7705ec-8c49-4eed-a56e-c21f3985254c/dah43cy-a8e121cb-934a-40f6-97c7-fa2d77130dd5.png/v1/fit/w_828,h_1148/pokemon_card_backside_in_high_resolution_by_atomicmonkeytcg_dah43cy-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQyMCIsInBhdGgiOiJcL2ZcLzRmNzcwNWVjLThjNDktNGVlZC1hNTZlLWMyMWYzOTg1MjU0Y1wvZGFoNDNjeS1hOGUxMjFjYi05MzRhLTQwZjYtOTdjNy1mYTJkNzcxMzBkZDUucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.9GzaYS7sd8RPY5FlHca09J9ZQZ9D9zI69Ru-BsbkLDA"
       //Append backOfPokeCard to the pokeCardDiv
       pokeCardDiv.appendChild(backOfPokeCard)
+
+      
     }
     kantoPokemon.append(pokeCardDiv)
   }

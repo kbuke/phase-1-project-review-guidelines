2331b9c71ee74d3f323a5fd6faec0fee84ffd0bd
kaan <kabuke13@gmail.com>
2023-09-23 10:09:23 +0100

    Creates the back of the pokemon card (with img). Need to append it to the pokeCardDiv

--- index.js
+++ index.js
@@ -32,11 +32,15 @@ document.addEventListener("DOMContentLoaded", () => {
       //Get the Pokemon Types, and add them to the class of each pokemon card
       let types = pokeInfo.types
       let specificType = types.map(pokeType => pokeType.type.name)
-
       //If there is more than one type, add them all to the classList
       for(let i = 0; i < specificType.length; i++){
         pokeCardDiv.classList.add(specificType[i])
       }
+
+      //Create the back of the Pokemon Card
+      let backOfPokeCard = document.createElement("img")
+      backOfPokeCard.className = "backEnd"
+      backOfPokeCard.src = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/4f7705ec-8c49-4eed-a56e-c21f3985254c/dah43cy-a8e121cb-934a-40f6-97c7-fa2d77130dd5.png/v1/fit/w_828,h_1148/pokemon_card_backside_in_high_resolution_by_atomicmonkeytcg_dah43cy-414w-2x.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9MTQyMCIsInBhdGgiOiJcL2ZcLzRmNzcwNWVjLThjNDktNGVlZC1hNTZlLWMyMWYzOTg1MjU0Y1wvZGFoNDNjeS1hOGUxMjFjYi05MzRhLTQwZjYtOTdjNy1mYTJkNzcxMzBkZDUucG5nIiwid2lkdGgiOiI8PTEwMjQifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.9GzaYS7sd8RPY5FlHca09J9ZQZ9D9zI69Ru-BsbkLDA"
     }
   }
 })
